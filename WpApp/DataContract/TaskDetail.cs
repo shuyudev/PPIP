bd3a51f1a5dca463068ac8af669fea7a484f1a06
lmy269 <lmy269@126.com>
2015-03-11 09:56:05 +0800

    add missed files

---
+++ WpApp/DataContract/TaskDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContract
+{
+    public class TaskDetail
+    {
+        public TaskType Type { get; set; }
+
+        public TaskInfo TaskInfo { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ WpApp/DataContract/TaskInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace DataContract
+{
+    public class TaskInfo
+    {
+        public string StorageAccountName { get; set; }
+        public string StorageAccountKey { get; set; }
+        public string BlobContainer { get; set; }
+        public string BlobName { get; set; }
+    }
+}
