04d69c3f28643e439e3ff6cc490f2792ff7e2be0
lmy269 <lmy269@126.com>
2015-03-11 10:02:20 +0800

    add missed files

---
+++ WpApp/UnitTestProject1/RemotePPIPServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using DataContract;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceContract;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RemotePPIPServiceTest
+    {
+        [TestMethod]
+        public void CanRegister()
+        {
+            Client client = new Client(false);
+            var deviceId = client.Register("miliu-lumia-123", "2028");
+
+            Assert.AreEqual("9", deviceId);
+        }
+
+        [TestMethod]
+        public void CanPullTask()
+        {
+            Client client = new Client(false);
+            List<TaskDetail> result = client.PullTask("9");
+
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
---
+++ WpApp/WpApp/Task/AppCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace WpApp.Task
+{
+    public static class AppCache
+    {
+        public const string DeviceIdName = "DeviceId";
+        public const string UploadedFilesName = "UploadedFiles";
+        public const string UploadTaskName = "UploadTask";
+
+        public static string DeviceId = "";
+        public static string UploadedFiles = "";
+        public static string UploadTask = "";
+
+        public static bool DevelopMode = false;
+
+
+        public static void ClearCache()
+        {
+            DeviceId = "";
+            UploadedFiles = "";
+            UploadTask = "";
+
+            ApplicationData.Current.LocalSettings.Values[DeviceIdName] = "";
+            ApplicationData.Current.LocalSettings.Values[UploadedFilesName] = "";
+            ApplicationData.Current.LocalSettings.Values[UploadTaskName] = "";
+        }
+    }
+}
